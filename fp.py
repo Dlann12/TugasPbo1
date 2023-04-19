130ecf6ae58f32b33e7e3fb0ad3da2200d852bab
Fadlan dwi Febrio <130965620+Dlann12@users.noreply.github.com>
2023-04-19 21:37:42 +0700

    Update fp.py

--- fp.py
+++ fp.py
@@ -3,7 +3,7 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 even_numbers = list(filter(lambda x: x % 2 == 0, numbers))
 print(even_numbers)
 
-# map function to double the numbers
+# map function to mengalikan 2 angka
 doubled_numbers = list(map(lambda x: x * 2, numbers))
 print(doubled_numbers)
 
