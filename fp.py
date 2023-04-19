77c26e77beba135dee6d8bf6452f6ead52be5b2d
Fadlan dwi Febrio <130965620+Dlann12@users.noreply.github.com>
2023-04-19 21:35:32 +0700

    Update fp.py

--- fp.py
+++ fp.py
@@ -1,4 +1,4 @@
-# filter function to return even numbers
+# filter function untuk mengembalikan nilai genap
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 even_numbers = list(filter(lambda x: x % 2 == 0, numbers))
 print(even_numbers)
@@ -7,7 +7,8 @@ print(even_numbers)
 doubled_numbers = list(map(lambda x: x * 2, numbers))
 print(doubled_numbers)
 
-# reduce function to find the sum of the numbers
+# reduce function untuk menghitung jumlah seluruh elemen dalam list
+from functools import reduce
 from functools import reduce
 sum_of_numbers = reduce(lambda x, y: x + y, numbers)
 print(sum_of_numbers)
