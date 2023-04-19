58df82b4d224d9f71abc3096fed248da6927107c
Fadlan dwi Febrio <130965620+Dlann12@users.noreply.github.com>
2023-04-19 21:49:40 +0700

    Update oop.py

--- oop.py
+++ oop.py
@@ -1,24 +1,24 @@
 class Animal:
-    def __init__(self, name, sound):
-        self.name = name
-        self.sound = sound
+    def __init__(type, name, sound):
+       type.name = name
+       type.sound = sound
 
-    def make_sound(self):
-        print(f"{self.name} makes {self.sound} sound")
+    def make_sound(type):
+        print(f"{type.name} makes {type.sound} sound")
 
 
 class Cat(Animal):
-    def __init__(self, name):
+    def __init__(type, name):
         super().__init__(name, "meow")
 
 
 class Dog(Animal):
-    def __init__(self, name):
+    def __init__(type, name):
         super().__init__(name, "bark")
 
 
 my_cat = Cat("Fluffy")
 my_dog = Dog("Buddy")
-
+# melakukan pemanggilan output setelah  membuat method method diatas 
 my_cat.make_sound()  # Output: Fluffy makes meow sound
 my_dog.make_sound()  # Output: Buddy makes bark sound
